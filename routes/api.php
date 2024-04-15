dfe7e09bd7b646d5b4176fa5de3b395b66f058a4
Henka No Jin <jose.mariovalentino@gmail.com>
2024-04-15 11:32:36 +0700

    Fix Review Controller Function & Routing [HNJ]

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,12 @@ Route::put('diskon/{id}/edit', [DiskonController::class, 'update']);
 
 Route::get('review', [ReviewController::class, 'index']);
 Route::get('review', [ReviewController::class, 'store']);
-Route::get('review/{id}/edit', [ReviewController::class, 'show']);
-Route::get('review/{id}/edit', [ReviewController::class, 'update']);
+Route::get('review/{id}', [ReviewController::class, 'show']);
+Route::get('review/{id}/edit', [DiskonController::class, 'edit']);
+Route::put('review/{id}/edit', [ReviewController::class, 'update']);
 
 Route::get('list-motor', [ListMotorController::class, 'index']);
 Route::get('list-motor', [ListMotorController::class, 'store']);
-Route::get('list-motor/{id}/edit', [ListMotorController::class, 'show']);
-Route::get('list-motor/{id}/edit', [ListMotorController::class, 'update']);
+Route::get('list-motor/{id}', [ListMotorController::class, 'show']);
+Route::get('list-motor/{id}/edit', [ListMotorController::class, 'edit']);
+Route::put('list-motor/{id}/edit', [ListMotorController::class, 'update']);
